7cfd13795b27d46fd5d9deee51e8d60bd13cb869
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-02-06 11:55:37 -0300

    adds copyright

--- hello_example/build_addon.sh
+++ hello_example/build_addon.sh
@@ -1,3 +1,7 @@
+# Copyright (c) 2016-2020 Knuth Project developers.
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 # Windows: Se necesita Python 2.7
 
 # npm install -g node-gyp
--- test.js
+++ test.js
@@ -1,3 +1,7 @@
+// Copyright (c) 2016-2020 Knuth Project developers.
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 // test.js
 
 // Windows: Se necesita Python 2.7 y MSVC
