85a84ed816573ca84fc6bff5670b27e3d775b970
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-02-20 21:24:26 -0300

    remove old directory

--- old/object_factory_example/build_addon.sh
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2016-2020 Knuth Project developers.
-# Distributed under the MIT software license, see the accompanying
-# file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
-# npm install -g node-gyp
-
-node-gyp configure
-# node-gyp build
-# node hello.js
