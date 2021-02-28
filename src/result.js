5bb2cb761a81bee0b93b2155c4d3df57ae93cfb6
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-02-27 21:40:42 -0300

    result helper

---
+++ src/result.js
@@ -0,0 +1,12 @@
+// Copyright (c) 2016-2021 Knuth Project developers.
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+class Result {
+    constructor(obj, ok) {
+        this.obj = obj;
+        this.ok = ok;
+    }
+}
+
+exports.Result = Result;
