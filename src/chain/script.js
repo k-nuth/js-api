1327fa8926008eee99afab12651f5612ac242c07
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-02-27 21:41:23 -0300

    implement script class

---
+++ src/chain/script.js
@@ -0,0 +1,58 @@
+// Copyright (c) 2016-2021 Knuth Project developers.
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+const kth = require('kth-bch-native');
+const memoize = require('memoizee');
+const result = require('../result');
+
+class Script {
+    constructor(encoded) {
+        this.encoded = encoded;
+    }
+
+    toNative(prefix) {
+        const native = kth.chain_script_construct(this.encoded, prefix);
+        return native;
+    }
+
+    rawData() {
+        const res = memoizedToData(this);
+        return res;
+    }
+}
+
+const fromNative = (native, prefix, destroy = false) => {
+    const obj = new Script(kth.chain_script_to_data(native, prefix));
+    if (destroy) {
+        destruct(native);
+    }
+    return obj;
+};
+
+const fromData = (encoded, prefix) => {
+    const native = kth.chain_script_construct(encoded, prefix);
+    const valid = kth.chain_script_is_valid(native);
+    const obj = fromNative(native, prefix);
+    destruct(native);
+    if (!valid) {
+        return new result.Result(undefined, false);
+    }
+    return new result.Result(obj, true);
+};
+
+const toData = (obj) => {
+    const res = obj.encoded;
+    return res;
+};
+
+const memoizedToData = memoize(toData);
+
+const destruct = (native) => {
+    kth.chain_script_destruct(native);
+};
+
+exports.fromNative = fromNative;
+exports.fromData = fromData;
+exports.destruct = destruct;
+exports.Script = Script;
