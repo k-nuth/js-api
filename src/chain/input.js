e4d2a997084f7b63932b12e4fff88d922362d4e0
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-03-03 11:15:22 -0300

    adds Input class

---
+++ src/chain/input.js
@@ -0,0 +1,73 @@
+// Copyright (c) 2016-2021 Knuth Project developers.
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+const kth = require('kth-bch-native');
+const memoize = require('memoizee');
+const script = require('./script');
+const outputPoint = require('./outputPoint');
+const result = require('../result');
+
+class Input {
+    constructor(previousOutpoint, script, sequence) {
+        this.previousOutpoint = previousOutpoint;
+        this.script = script;
+        this.sequence = sequence;
+    }
+
+    toNative() {
+        const native = kth.chain_input_construct(
+            this.previousOutpoint.toNative(),
+            this.script.toNative(false),
+            this.sequence
+        );
+        return native;
+    }
+
+    rawData(wire = true) {
+        const res = memoizedToData(this, wire);
+        return res;
+    }
+}
+
+const fromNative = (native, destroy = false) => {
+    const obj = new Input(
+        outputPoint.fromNative(kth.chain_input_previous_output(native)),
+        script.fromNative(kth.chain_input_script(native), false),
+        kth.chain_input_sequence(native)
+    );
+    if (destroy) {
+        destruct(native);
+    }
+    return obj;
+};
+
+const fromData = (data) => {
+    const native = kth.chain_input_factory_from_data(data);
+    const valid = kth.chain_input_is_valid(native);
+    if (!valid) {
+        destruct(native);
+        return new result.Result(undefined, false);
+    }
+    const obj = fromNative(native);
+    destruct(native);
+    return new result.Result(obj, true);
+};
+
+const toData = (obj, wire) => {
+    const native = obj.toNative();
+    const res = kth.chain_input_to_data(native, wire);
+    destruct(native);
+    return res;
+};
+
+const memoizedToData = memoize(toData);
+
+const destruct = (native) => {
+    kth.chain_input_destruct(native);
+};
+
+exports.fromNative = fromNative;
+exports.fromData = fromData;
+exports.destruct = destruct;
+exports.Input = Input;
