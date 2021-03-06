aae0a4fa967ddefc123bbe68b2f66ffdf86831f2
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-03-05 23:48:11 -0300

    database settings tests

---
+++ test/config/databaseSettings.test.js
@@ -0,0 +1,14 @@
+const databaseSettings = require('../../src/config/databaseSettings');
+const network = require('../../src/config/network');
+
+test('read default settings', () => {
+    const settings = databaseSettings.getDefault(network.network.mainnet);
+    expect(settings.directory).toBe('blockchain');
+    expect(settings.flushWrites).toBe(false);
+    expect(settings.fileGrowthRate).toBe(50);
+    expect(settings.indexStartHeight).toBe(0);
+    expect(settings.reorgPoolLimit).toBe(100);
+    expect(settings.dbMaxSize).toBe(600 * 1024 * 1024 * 1024);
+    expect(settings.safeMode).toBe(true);
+    expect(settings.cacheCapacity).toBe(0);
+});
