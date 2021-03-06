6357373a07a9259c68dbbd25bde0249b0020e183
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-03-06 20:49:16 -0300

    node settings tests

---
+++ test/config/nodeSettings.test.js
@@ -0,0 +1,11 @@
+const nodeSettings = require('../../src/config/nodeSettings');
+const network = require('../../src/config/network');
+
+test('read default settings', () => {
+    const settings = nodeSettings.getDefault(network.network.mainnet);
+    expect(settings.syncPeers).toBe(0);
+    expect(settings.syncTimeoutSeconds).toBe(5);
+    expect(settings.blockLatencySeconds).toBe(60);
+    expect(settings.refreshTransactions).toBe(true);
+    expect(settings.compactBlocksHighBandwidth).toBe(true);
+});
