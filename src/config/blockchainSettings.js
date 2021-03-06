702b1b96ced552c8bce80559e73de7ec7160c541
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-03-05 23:48:24 -0300

    blockchain settings

---
+++ src/config/blockchainSettings.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2016-2021 Knuth Project developers.
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+const kth = require('kth-bch-native');
+
+const getDefault = (network) => {
+    return kth.config_blockchain_settings_default(network);
+};
+
+exports.getDefault = getDefault;
