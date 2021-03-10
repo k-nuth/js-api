6cea0dc84bbbf0d2c4d0acb9dd0969ecdc0172e0
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-03-10 20:28:49 -0300

    primitive helper types

---
+++ src/primitives.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2016-2021 Knuth Project developers.
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+const startModules = Object.freeze({
+    all: 0,
+    justChain: 1,
+    justP2P: 2
+});
+
+exports.startModules = startModules;
