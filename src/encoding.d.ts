60d031c5434b9d76814c33ca1e4747edb4595f5d
nicocappabianca <nicolas.cappabianca@gmail.com>
2021-03-10 21:19:06 -0300

    add copyright and numerics.d.ts

--- src/encoding.d.ts
+++ src/encoding.d.ts
@@ -1,3 +1,7 @@
+// Copyright (c) 2016-2021 Knuth Project developers.
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 export declare function bytesToHexStr(uint8arr: Uint8Array): string; 
 
 export declare function hexStrToBytes(str: string): Uint8Array;
