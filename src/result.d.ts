b33f42f92660e8e57a583e5b04ee06994cc8c404
nicocappabianca <nicolas.cappabianca@gmail.com>
2021-03-24 17:01:36 -0300

    add copyright & TODO comments

--- src/result.d.ts
+++ src/result.d.ts
@@ -1,3 +1,9 @@
+// Copyright (c) 2016-2021 Knuth Project developers.
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 export declare class Result { 
   constructor(obj: any, ok: boolean); 
 }
+
+// TODO (Nicolás): Check with Fernando all 'any' types 
