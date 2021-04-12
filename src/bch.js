23f450f02e9f011eaca29d518293fca342ec418e
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-04-12 10:23:27 -0300

    new index

---
+++ src/bch.js
@@ -0,0 +1,38 @@
+// Copyright (c) 2016-2021 Knuth Project developers.
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+const node = require('./node');
+const enc = require('./encoding');
+
+const block = require('./chain/block');
+const header = require('./chain/header');
+const input = require('./chain/input');
+const output = require('./chain/output');
+const outputPoint = require('./chain/outputPoint');
+const script = require('./chain/script');
+const transaction = require('./chain/transaction');
+
+const settings = require('./config/settings');
+const sett_network = require('./config/network');
+
+class BCH {
+    constructor() {
+        this.node = node;
+        this.enc = enc;
+
+        this.block = block;
+        this.header = header;
+        this.input = input;
+        this.output = output;
+        this.outputPoint = outputPoint;
+        this.script = script;
+        this.transaction = transaction;
+
+        this.settings = settings;
+        this.network = sett_network.network;
+        // const setts = settings.getDefault(network.network.mainnet);
+    }
+}
+
+module.exports = new BCH();
