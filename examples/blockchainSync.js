26df5d34562f030809e76942c395c1bd82463dd8
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-04-14 12:44:17 -0300

    fix example

--- examples/blockchainSync.js
+++ examples/blockchainSync.js
@@ -39,7 +39,7 @@ async function comeBackAfterTheBCHHardFork(node) {
 
 function shutdown() {
     console.log('graceful shutdown ...');
-    running_ = true;
+    running_ = false;
 }
 
 function sleep(ms) {
