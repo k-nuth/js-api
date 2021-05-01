510544576f0b477c3044472f2d5184abccf56195
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-05-01 20:58:37 -0300

    removes spaces

--- src/chain/header.d.ts
+++ src/chain/header.d.ts
@@ -2,24 +2,24 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-import { Result } from '../result'; 
+import { Result } from '../result';
 
-export declare class Header { 
-  constructor(version: number, previousBlockHash: Uint8Array, merkle: Uint8Array, timestamp: number , bits: number, nonce: number); 
+export declare class Header {
+  constructor(version: number, previousBlockHash: Uint8Array, merkle: Uint8Array, timestamp: number , bits: number, nonce: number);
 
   toNative(): any;
 
-  get hash(): Uint8Array; 
+  get hash(): Uint8Array;
 
-  rawData(version: number): Uint8Array; 
+  rawData(version: number): Uint8Array;
 }
 
-export declare function fromNative(native: any, destroy: boolean): Header; 
+export declare function fromNative(native: any, destroy: boolean): Header;
 
-export declare function fromData(version: number, data: Uint8Array): Result<Header>; 
+export declare function fromData(version: number, data: Uint8Array): Result<Header>;
 
-export declare function toData(obj: Header, version: number): Uint8Array; 
+export declare function toData(obj: Header, version: number): Uint8Array;
 
-export declare function hash(obj: Header): Uint8Array; 
+export declare function hash(obj: Header): Uint8Array;
 
-export declare function destruct(native: any): void; 
+export declare function destruct(native: any): void;
