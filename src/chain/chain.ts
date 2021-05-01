7a4a638e1862cba368bc9e92fc9731f14d5141cb
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-05-01 20:58:12 -0300

    adds TS Chain API

---
+++ src/chain/chain.ts
@@ -0,0 +1,27 @@
+// Copyright (c) 2016-2021 Knuth Project developers.
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+import { Block } from "./block";
+import { Header } from "./header";
+import { Transaction } from "./transaction";
+
+export declare class Chain {
+    constructor(native : any);
+
+    getLastHeight() : Promise<[number, Header]>;
+    getBlockHeight(hash : Uint8Array) : Promise<[number, Header]>;
+
+    getBlockHeaderByHeight(height : number) : Promise<[number, Header, number]>;
+    getBlockHeaderByHash(hash : Uint8Array) : Promise<[number, Header, number]>;
+
+    getBlockByHeight(height : number) : Promise<[number, Block, number]>;
+    getBlockByHash(hash : Uint8Array) : Promise<[number, Block, number]>;
+
+    getTransaction(hash : Uint8Array, requireConfirmed : boolean) : Promise<[number, Transaction, number, number]>;
+    getTransactionPosition(hash : Uint8Array, requireConfirmed : boolean) : Promise<[number, number, number]>;
+
+    // Organizers
+    organizeBlock(block : Block) : Promise<number>;
+    organizeTransaction(transaction : Transaction) : Promise<number>;
+}
