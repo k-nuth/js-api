6185a84e805a309ffc3779a340bfe46d2a13384c
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-05-01 20:59:05 -0300

    Settings API for TS

---
+++ src/config/settings.ts
@@ -0,0 +1,148 @@
+// Copyright (c) 2016-2021 Knuth Project developers.
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+import { Network } from './network';
+
+export interface Checkpoint {
+    hash: Uint8Array;
+    height: number;
+};
+
+export interface Authority {
+    ip: string;
+    port: number;
+};
+
+export interface Endpoint {
+    scheme: string;
+    host: string;
+    port: number;
+};
+
+export interface NodeSettings {
+    syncPeers: number;
+    syncTimeoutSeconds: number;
+    blockLatencySeconds: number;
+    refreshTransactions: boolean;
+    compactBlocksHighBandwidth: boolean;
+};
+
+export interface BlockchainSettings {
+    cores: number;
+    priority: boolean;
+    byteFeeSatoshis: number;
+    sigopFeeSatoshis: number;
+    minimumOutputSatoshis: number;
+
+    notifyLimitHours: number;
+    reorganizationLimit: number;
+
+    checkpoints: [Checkpoint];
+
+    fixCheckpoints: boolean;
+    allowCollisions: boolean;
+    easyBlocks: boolean;
+    retarget: boolean;
+    bip16: boolean;
+    bip30: boolean;
+    bip34: boolean;
+    bip66: boolean;
+    bip65: boolean;
+    bip90: boolean;
+    bip68: boolean;
+    bip112: boolean;
+    bip113: boolean;
+    bchUahf: boolean;
+    bchDaaCw144: boolean;
+    bchPythagoras: boolean;
+    bchEuclid: boolean;
+
+    bchPisano: boolean;
+    bchMersenne: boolean;
+    bchFermat: boolean;
+    bchEuler: boolean;
+    bchGauss: boolean;
+
+    eulerActivationTime: number;
+    gaussActivationTime: number;
+    asertHalfLife: number;
+};
+
+export interface DatabaseSettings {
+    directory: string;
+    flushWrites: boolean;
+    fileGrowthRate: number;
+    indexStartHeight: number;
+    reorgPoolLimit: number;
+    dbMaxSize: number;
+    safeMode: boolean;
+    cacheCapacity: number;
+};
+
+export interface NetworkSettings {
+    threads: number;
+
+    protocolMaximum: number;
+    protocolMinimum: number;
+    services: number;
+    invalidServices: number;
+
+    relayTransactions: boolean;
+    validateChecksum: boolean;
+
+    identifier: number;
+    inboundPort: number;
+    inboundConnections: number;
+    outboundConnections: number;
+    manualAttemptLimit: number;
+    connectBatchSize: number;
+    connectTimeoutSeconds: number;
+    channelHandshakeSeconds: number;
+    channelHeartbeatMinutes: number;
+    channelInactivityMinutes: number;
+    channelExpirationMinutes: number;
+    channelGerminationSeconds: number;
+    hostPoolCapacity: number;
+
+    hostsFile: string;
+
+    self: Authority;
+    blacklist: [Authority];
+
+    peers: [Endpoint];
+    seeds: [Endpoint];
+
+    debugFile: string;
+    errorFile: string;
+    archiveDirectory: string;
+
+    rotationSize: number;
+    minimumFreeSpace: number;
+    maximumArchiveSize: number;
+    maximumArchiveFiles: number;
+
+    statisticsServer: Authority;
+
+    verbose: boolean;
+    useIpv6: boolean;
+
+    userAgentBlacklist: [string];
+};
+
+export interface Settings {
+    node: NodeSettings;
+    chain: BlockchainSettings;
+    database: DatabaseSettings;
+    network: NetworkSettings;
+};
+
+export interface SettingsResult {
+    ok: boolean;
+    message?: string;
+    settings?: Settings;
+};
+
+export declare function getDefault(network : Network): Settings;
+
+export declare function getFromFile(file : string): SettingsResult;
