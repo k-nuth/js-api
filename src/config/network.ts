5d6f03b3800030a13b5a588393bc727a838f5f5c
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-05-01 20:58:54 -0300

    Network enum for TS

---
+++ src/config/network.ts
@@ -0,0 +1,11 @@
+// Copyright (c) 2016-2021 Knuth Project developers.
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+export enum Network {
+    mainnet = 0,
+    testnet = 1,
+    regtest = 2,
+    testnet4 = 3,
+    scalenet = 4
+}
