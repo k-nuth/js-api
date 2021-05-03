941642eed0a8aef52d8b81c4c5ff3e48d9c83abe
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-05-03 16:12:35 -0300

    adds kth general module for TS

---
+++ src/kth.d.ts
@@ -0,0 +1,38 @@
+// Copyright (c) 2016-2021 Knuth Project developers.
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+import * as node from './node';
+
+import * as enc from './encoding';
+import { StartModules as startModules } from './primitives';
+
+import * as block from './chain/block';
+import * as header from './chain/header';
+import * as input from './chain/input';
+import * as output from './chain/output';
+import * as outputPoint from './chain/outputPoint';
+import * as script from './chain/script';
+import * as transaction from './chain/transaction';
+
+import * as settings from './config/settings';
+import { Network as network } from './config/network';
+
+export {
+    node,
+    enc,
+    startModules,
+
+    //Chain
+    block,
+    header,
+    input,
+    output,
+    outputPoint,
+    script,
+    transaction,
+
+    //Config
+    settings,
+    network
+}
