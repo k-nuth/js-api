13d0d11bf444af9caab3d1cd82a2b0d21f92c751
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-05-31 10:16:42 -0300

    encoding example

---
+++ examples/encoding.js
@@ -0,0 +1,10 @@
+// Copyright (c) 2016-2021 Knuth Project developers.
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+/* eslint-disable */
+
+const enc = require('../src/encoding');
+
+const hash = enc.Hash.strToBytes('000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f')
+console.log(hash);
