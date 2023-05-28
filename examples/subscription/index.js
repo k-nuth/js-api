7a614dadd2fbb245060a0c1cd238f87d3fde4f20
Fernando Pelliccioni <fpelliccioni@gmail.com>
2023-05-28 21:54:51 +0200

    improve subscription examples

--- examples/subscription/index.js
+++ examples/subscription/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+
+app.use(express.static(path.join(__dirname, '.'))); // to serve your static files
 
 let blockHeight = 0;
 
