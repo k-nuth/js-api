d16fd20171ee2dc152da9f9c8276e17385f16999
Fernando Pelliccioni <fpelliccioni@gmail.com>
2023-08-12 14:50:35 +0200

    fix tests (#49)

--- test/node.test.js
+++ test/node.test.js
@@ -124,7 +124,7 @@ describe('full indexed node tests', () => {
     it('node information', async () => {
         expect(node_.capi_version).toEqual("0.39.0");
         expect(node_.cppapi_version).toEqual("0.38.0");
-        expect(node_.version).toEqual("2.0.0");
+        expect(node_.version).toEqual("2.1.1");
         expect(node_.microarchitecture).toEqual("ZLm9Pjh");
         expect(node_.march_names).toEqual("64 bits, CMOV, CX8, FPU, FXSR, MMX, SCE, SSE, SSE2, CX16, LAHF-SAHF, POPCNT, SSE3, SSE4.1, SSE4.2, SSSE3, AVX, AVX2, BMI1, BMI2, F16C, FMA, LZCNT ABM, MOVBE, XSAVE");
         expect(node_.currency_symbol).toEqual("BCH");
