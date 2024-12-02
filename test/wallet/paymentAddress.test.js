fdcf7eb092621d1de6f26239554bb647821a3c50
Fernando Pelliccioni <fpelliccioni@gmail.com>
2024-12-02 19:30:10 +0100

    feat: bump js-native version

--- test/wallet/paymentAddress.test.js
+++ test/wallet/paymentAddress.test.js
@@ -4,22 +4,22 @@
 
 const paymentAddress = require('../../src/wallet/paymentAddress');
 
-test('EmptyAddressShouldFail', () => {
+it('should fail when address is empty', () => {
     const result = paymentAddress.fromString('');
     expect(result.ok).toBe(false);
 });
 
-test('WhitespaceAddressShouldFail', () => {
+it('should fail when address is whitespace', () => {
     const result = paymentAddress.fromString(' ');
     expect(result.ok).toBe(false);
 });
 
-test('InvalidAddressShouldFail', () => {
+it('should fail when address is invalid', () => {
     const result = paymentAddress.fromString('abcd');
     expect(result.ok).toBe(false);
 });
 
-test('MainnetCashAddrAddressOK', () => {
+it('should handle mainnet cashaddr address correctly', () => {
     const isVal = paymentAddress.isValid('bitcoincash:qrcuqadqrzp2uztjl9wn5sthepkg22majyxw4gmv6p');
     expect(isVal).toBe(true);
 
@@ -32,7 +32,7 @@ test('MainnetCashAddrAddressOK', () => {
     expect(addr.encodedLegacy()).toBe('1P3GQYtcWgZHrrJhUa4ctoQ3QoCU2F65nz');
 });
 
-test('MainnetCashAddrNoPrefixAddressOK', () => {
+it('should handle mainnet cashaddr address without prefix correctly', () => {
     const isVal = paymentAddress.isValid('qrcuqadqrzp2uztjl9wn5sthepkg22majyxw4gmv6p');
     expect(isVal).toBe(true);
 
@@ -45,7 +45,7 @@ test('MainnetCashAddrNoPrefixAddressOK', () => {
     expect(addr.encodedLegacy()).toBe('1P3GQYtcWgZHrrJhUa4ctoQ3QoCU2F65nz');
 });
 
-test('MainnetLegacyAddressOK', () => {
+it('should handle mainnet legacy address correctly', () => {
     const isVal = paymentAddress.isValid('1P3GQYtcWgZHrrJhUa4ctoQ3QoCU2F65nz');
     expect(isVal).toBe(true);
 
@@ -56,4 +56,16 @@ test('MainnetLegacyAddressOK', () => {
     expect(addr.encodedCashAddr()).toBe('bitcoincash:qrcuqadqrzp2uztjl9wn5sthepkg22majyxw4gmv6p');
     expect(addr.encodedCashTokens()).toBe('bitcoincash:zrcuqadqrzp2uztjl9wn5sthepkg22majypyxk429j');
     expect(addr.encodedLegacy()).toBe('1P3GQYtcWgZHrrJhUa4ctoQ3QoCU2F65nz');
+});
+
+it('should handle 32-byte CashAddr correctly for mainnet', () => {
+    const isVal = paymentAddress.isValid('bitcoincash:pvstqkm54dtvnpyqxt5m5n7sjsn4enrlxc526xyxlnjkaycdzfeu69reyzmqx');
+    expect(isVal).toBe(true);
+
+    const result = paymentAddress.fromString('bitcoincash:pvstqkm54dtvnpyqxt5m5n7sjsn4enrlxc526xyxlnjkaycdzfeu69reyzmqx');
+    expect(result.ok).toBe(true);
+    const addr = result.obj;
+    expect(addr.encodedCashAddr()).toBe('bitcoincash:pvstqkm54dtvnpyqxt5m5n7sjsn4enrlxc526xyxlnjkaycdzfeu69reyzmqx');
+    expect(addr.encodedCashTokens()).toBe('bitcoincash:rvstqkm54dtvnpyqxt5m5n7sjsn4enrlxc526xyxlnjkaycdzfeu6hs99m6ed');
+    expect(addr.encodedLegacy()).toBe('34frpCV2v6wtzig9xx4Z9XJ6s4jU3zqwR7');// In fact a 32-byte address is not representable in legacy encoding.
 });
